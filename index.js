e6a616fbc62bae1e16da06787d7d86a2879f50e0
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-06-19 13:48:10 +0600

    added jwt verification logic

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
+import { createRemoteJWKSet, jwtVerify } from "jose";
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,36 @@ const port = process.env.PORT || 5001;
 // MIDDLEWARES
 app.use(cors());
 app.use(express.json());
+
+const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req?.headers.authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    const token = authHeader.split(' ')[1]
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    // console.log(token);
+    try {
+        const { payload } = await jwtVerify(token, JWKS)
+        // console.log('server/jwt/payload: ', payload);
+        req.user = payload;
+        next()
+
+
+    } catch (error) {
+        return res.status(403).json({ message: 'Forbidden' });
+    }
+
+
+}
+
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -36,6 +67,13 @@ async function run() {
         const database = client.db("BookVerse");
         // const facilitiesCollection = database.collection("facilities");
 
+
+        // app.get('/protected-message', verifyToken, async (req, res) => {
+        //     // res.send(result);
+        //     res.json({ message: 'protected messaged accessed' });
+
+
+        // });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
